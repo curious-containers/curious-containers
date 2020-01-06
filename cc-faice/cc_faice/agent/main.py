28bd3fc747c2e3a731a64297b8325b8541200ed1
Bluemi <bruno.schilling@protonmail.ch>
2020-01-06 13:28:33 +0100

    removed deprecated agent red imports

--- cc-faice/cc_faice/agent/main.py
+++ cc-faice/cc_faice/agent/main.py
@@ -1,19 +1,2 @@
-from collections import OrderedDict
-
-from cc_faice.agent.red.main import main as red_main
-from cc_faice.agent.red.main import DESCRIPTION as RED_DESCRIPTION
-
-# from cc_core.commons.cli_modes import cli_modes
-
-
-SCRIPT_NAME = 'faice agent'
-TITLE = 'modes'
-DESCRIPTION = 'Run a RED experiment.'
-MODES = OrderedDict([
-    ('red', {'main': red_main, 'description': RED_DESCRIPTION}),
-])
-
-
 def main():
     raise DeprecationWarning('faice agent red is deprecated. Use faice exec with ccfaice execution engine instead.')
-    # cli_modes(SCRIPT_NAME, TITLE, DESCRIPTION, MODES)
