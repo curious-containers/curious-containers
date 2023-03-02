11fdee95278acd6b7efeebdbe4a81bebbf0d00a3
yash revannavar <yashrevannavar13@gmail.com>
2023-03-02 09:20:47 +0100

    Changes done as mentioned also changed the function nae from read_file >> red_create.

--- cc-faice/tests/commons/test_demo.py
+++ cc-faice/tests/commons/test_demo.py
@@ -1,10 +1,10 @@
 from ruamel.yaml import YAML
-from cc_faice.exec.main import _has_outputs
+from cc_faice.exec.main import _has_outputs, _check_execution_arguments
 
 yaml = YAML(typ='safe')
 
 # Reading all the Red files
-def read_file(batch:bool,output:bool):
+def red_create(batch:bool,output:bool):
     """
     This fucntion creates a red file for testing pupose.
     :param batch: If the batch is required
@@ -16,17 +16,17 @@ def read_file(batch:bool,output:bool):
     """
     with open("tests/red_files/initial.red",'r') as f:
         initial_red = yaml.load(f)
-    if output == True:
+    if output:
         with open("tests/red_files/outputs.red",'r') as f:
             output_red = yaml.load(f)
     else:
         with open("tests/red_files/no_outputs.red",'r') as f:
             output_red = yaml.load(f)        
-    if (output == True) & (batch == True):
+    if output and batch:
         initial_red['batches'] = output_red
-    elif (output == False) & (batch == True):
+    elif not output and batch:
         initial_red['batches'] = output_red
-    elif (output == True) & (batch == False):
+    elif output and not batch:
         initial_red.update(output_red)
     else:
         initial_red.update(output_red)
@@ -37,5 +37,5 @@ def test_has_outputs():
     """
     Tests the _has_outputs function from cc_faice.exec.main for two red files. 
     """
-    assert not _has_outputs(read_file(batch=False,output=False)), 'Expected no outputs but found one output in hello_world.red'
-    assert  _has_outputs(read_file(batch=False,output=True)), 'Expected outputs but not found output in input_output.red'
+    assert not _has_outputs(red_create(batch=False,output=False)), 'Expected no outputs but found one output in hello_world.red'
+    assert  _has_outputs(red_create(batch=False,output=True)), 'Expected outputs but not found output in input_output.red'
