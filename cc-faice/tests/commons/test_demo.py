04d45ec8f232c0c17a7aaecffa097da9da8217d1
yash revannavar <yashrevannavar13@gmail.com>
2023-03-16 09:47:47 +0100

    added 2 new red files and test_check_execution_arguments

--- cc-faice/tests/commons/test_demo.py
+++ cc-faice/tests/commons/test_demo.py
@@ -1,4 +1,6 @@
 from ruamel.yaml import YAML
+import pytest
+from cc_core.commons.exceptions import InvalidExecutionEngineArgumentException
 from cc_faice.exec.main import _has_outputs, _check_execution_arguments
 
 yaml = YAML(typ='safe')
@@ -16,6 +18,10 @@ def red_create(batch: bool, output: bool):
     """
     with open("tests/red_files/initial.red",'r') as f:
         initial_red = yaml.load(f)
+    with open("tests/red_files/container.red",'r') as f:
+        container_red = yaml.load(f)
+    with open("tests/red_files/execution.red",'r') as f:
+        execution_red = yaml.load(f)
     if output:
         with open("tests/red_files/outputs.red",'r') as f:
             output_red = yaml.load(f)
@@ -30,6 +36,8 @@ def red_create(batch: bool, output: bool):
         initial_red.update(output_red)
     else:
         initial_red.update(output_red)
+    initial_red.update(container_red)
+    initial_red.update(execution_red)
     return initial_red
 
 
@@ -39,3 +47,33 @@ def test_has_outputs():
     """
     assert not _has_outputs(red_create(batch= False, output= False)), 'Expected no outputs but found one output in hello_world.red'
     assert  _has_outputs(red_create(batch= False, output= True)), 'Expected outputs but not found output in input_output.red'
+
+
+ALLOWED_EXECUTION_ENGINE_ARGUMENTS = {
+    'ccfaice': [
+        'preserve_environment', 'disable_pull', 'leave_container', 'insecure', 'gpu_ids', 'disable_connector_validation'
+    ],
+    'ccagency': ['disable_retry', 'disable_connector_validation']
+}
+
+def test_check_execution_arguments():
+    """
+    Tests the _check_execution_arguments function from cc_faice.exec.main. 
+    """
+    _check_execution_arguments('ccagency',disable_retry= True, disable_connector_validation= True)
+    _check_execution_arguments('ccfaice',leave_container= True)
+    _check_execution_arguments('ccfaice',preserve_environment= True)
+    _check_execution_arguments('ccfaice',gpu_ids= True)
+    _check_execution_arguments('ccfaice',disable_pull= True)
+    with pytest.raises(InvalidExecutionEngineArgumentException):
+        _check_execution_arguments('ccfaice',disable_retry= True, disable_connector_validation= True)
+    with pytest.raises(InvalidExecutionEngineArgumentException):
+        _check_execution_arguments('ccagency',leave_container= True)
+    with pytest.raises(InvalidExecutionEngineArgumentException): 
+        _check_execution_arguments('ccagency',preserve_environment= True)
+    with pytest.raises(InvalidExecutionEngineArgumentException): 
+        _check_execution_arguments('ccagency',gpu_ids= True)
+    with pytest.raises(InvalidExecutionEngineArgumentException): 
+        _check_execution_arguments('ccagency',insecure= True)    
+    with pytest.raises(InvalidExecutionEngineArgumentException): 
+        _check_execution_arguments('ccagency',disable_pull= True)    
