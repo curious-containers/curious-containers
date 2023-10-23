dfdeeee7352d488f4a44b7d25336f2742af5c7d9
Dominik Probst <mail@d-probst.de>
2023-10-23 13:39:41 +0000

    build docker image based on source code

--- tests/setup/build_docker_images.sh
+++ tests/setup/build_docker_images.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-docker build -t "cc_broker:latest" "../cc-agency/docker/image_agency_broker"
-docker build -t "cc_controller:latest" "../cc-agency/docker/image_agency_controller"
-docker build -t "cc_trustee:latest" "../cc-agency/docker/image_agency_trustee"
-docker build -t "cc_mongodb:latest" "../cc-agency/docker/image_mongodb"
+cd ..
+
+docker build -t "cc_broker:latest" -f "tests/setup/image_agency_broker/Dockerfile" .
+docker build -t "cc_controller:latest" -f "tests/setup/image_agency_controller/Dockerfile" .
+docker build -t "cc_trustee:latest" -f "tests/setup/image_agency_trustee/Dockerfile" .
+docker build -t "cc_mongodb:latest" -f "tests/setup/image_mongodb/Dockerfile" .
